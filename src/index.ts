003abf017ded2e232d7b0e01e45e2665909a06aa
ayodoubleayo <ayodoubleayo@gmail.com>
2026-01-09 02:33:52 -0800

    Fix ESM imports and CORS headers

--- src/index.ts
+++ src/index.ts
@@ -70,7 +70,8 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization" ,       "x-request-id",
+ ],
   })
 );
 
